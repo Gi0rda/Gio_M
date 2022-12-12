1f366e5dde7988fe4392d0c869c882b661aea036
Gi0rda <96236823+Gi0rda@users.noreply.github.com>
2022-12-12 09:08:59 +0100

    Add files via upload

---
+++ wait.c
@@ -0,0 +1,18 @@
+#include <sys/wait.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+int main(){
+	int status;
+	int pid = fork();
+	if(pid == 0){
+		printf("Sono il figlio, il mio pid è: %d. ", getpid());
+		printf("Il mio papi ha pid: %d\n", getpid());
+		exit(69);
+}
+else{
+	printf("Sono il padre, il mio pid è : %d.", getpid());
+	printf("L'exit di mio figlio (%d) è : %d\n", wait(&status), status);
+	return 0;
+	}
+}
